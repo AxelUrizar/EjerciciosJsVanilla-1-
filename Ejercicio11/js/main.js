8ba09f4005e2e2ba0f0ca359402439ae360993c8
AxelUrizar <axelurizarmartin@gmail.com>
2021-12-16 18:32:46 +0100

    Ejercicio 11

---
+++ Ejercicio11/js/main.js
@@ -0,0 +1,64 @@
+let dia = prompt('Escribe un dia de la semana')
+
+
+switch (dia) {
+    case 'sábado':
+        alert('Festivo');
+        break;
+        
+    case 'Sábado':
+        alert('Festivo');
+        break;
+    
+    case 'domingo':
+        alert('Festivo');
+        break;
+    
+    case 'Domingo':
+        alert('Festivo');
+        break;
+
+    case 'lunes':
+        alert('laboral');
+        break;
+    
+    case 'Lunes':
+        alert('laboral');
+        break;
+
+    case 'martes':
+        alert('laboral');
+        break;
+
+    case 'Martes':
+        alert('laboral');
+        break;
+
+    case 'miércoles':
+        alert('laboral');
+        break;
+
+    case 'Miércoles':
+        alert('laboral');
+        break;
+
+    case 'jueves':
+        alert('laboral');
+        break;
+
+    case 'Jueves':
+        alert('laboral');
+        break;
+
+    case 'viernes':
+        alert('laboral');
+        break;
+
+    case 'Viernes':
+        alert('laboral');
+        break;
+
+    default:
+        console.log('No cometas faltas de ortografia');
+        break;
+}
