fe96fa9136165fba12d14ce0fb17d1a80335a9a5
AxelUrizar <axelurizarmartin@gmail.com>
2021-12-16 17:15:57 +0100

    Ejercicio1

---
+++ Ejercicio1/js/main.js
@@ -0,0 +1,14 @@
+x = 1
+y = 3
+
+if ( x > y) {
+    document.write(`${x} es mayor que ${y}`)
+}
+
+else if (y > x) {
+    document.write(`${y} es mayor que ${x}`)
+}
+
+else{
+    document.write(`${y} es igual a ${x}`)
+}
