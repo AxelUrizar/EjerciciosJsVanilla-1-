31ed2540a85350a1379f755ff9fd179f6863c60f
AxelUrizar <axelurizarmartin@gmail.com>
2021-12-17 11:07:13 +0100

    ejercicio 10 moificado

--- Ejercicio10/js/main.js
+++ Ejercicio10/js/main.js
@@ -1,10 +1,13 @@
 let ventas = parseFloat(prompt('Cuantas cosas has comprado?'));
 let valor= [];
-let valorFinal = ''
+
 for (let i = ventas; i > 0; i--) {
     valorObjeto = parseFloat(prompt(`Cuanto costaba el elemento ${i}`));
-    valorFinal = valor.push(valorObjeto);
+    valor.push(valorObjeto);
 }
 
-alert(valor.reduce((a, b) => a + b));
+
+valor.reduce((a, b) => a + b);
+
+console.log(valor)
 
